3993366f097f883910fe69ef0e73e2918fa1c051
Stephen H Bruen <52644685+stephenbruen@users.noreply.github.com>
2023-02-19 09:56:56 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const myArray = [2, 3, 4];
+const newValue = 1;
+
+myArray.unshift(newValue);
+console.log(myArray); // Output: [1, 2, 3, 4]
+
+
+const myArray = [1, 2, 3];
+
+const firstValue = myArray.shift();
+console.log(firstValue); // Output: 1
+console.log(myArray); // Output: [2, 3]
+
+
+const myArray = [1, 2, 3];
+const index = 1;
+const newValue = 4;
+
+myArray.splice(index, 0, newValue);
+console.log(myArray); // Output: [1, 4, 2, 3]
+
+
+const myArray = [1, 2, 3, 4];
+const index = 1;
+
+const removedValue = myArray.splice(index, 1)[0];
+console.log(removedValue); // Output: 2
+console.log(myArray); // Output: [1, 3, 4]
